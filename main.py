415f3328791c1f2b54224d9a447bb0be2b17a371
moonsung1234 <dit67890@gmail.com>
2022-08-21 04:55:15 +0900

    test

--- main.py
+++ main.py
@@ -1,47 +1,5 @@
 
-from bs4 import BeautifulSoup
-import numpy as np
-import requests
+from model import Model
 
-url = "https://www.koreabaseball.com/Record/Player/HitterBasic/Basic1.aspx"
-new_url = "https://www.koreabaseball.com"
-new_url_list = []
-
-data = np.array([])
-
-res = requests.get(url)
-
-if res.status_code == 200 :
-    html = res.text
-    soup = BeautifulSoup(html, "html.parser")
-    
-    tr = soup.select("#cphContents_cphContents_cphContents_udpContent > div.record_result > table > tbody > tr")
-
-    for e in tr :
-        href = e.select_one("td:nth-child(2) > a")["href"]
-
-        new_url_list.append(new_url + href.replace("Basic", "Total"))
-
-for new_url in new_url_list :
-    res = requests.get(new_url)
-
-    id = new_url[-5:]
-
-    if res.status_code == 200 :
-        html = res.text
-        soup = BeautifulSoup(html, "html.parser")
-
-        tr = soup.select("#contents > div.sub-content > div.player_records > div > table > tbody > tr")
-
-        for e in tr :
-            td = e.select("td")[2:]
-            td = list(map(lambda x : x.get_text().replace("-", "0"), td))
-
-            td.insert(0, id)
-
-            td = np.round(np.array(td, dtype=np.float).reshape(1, -1), 3)
-            data = td if len(data) == 0 else np.concatenate((data, td), axis=0)
-
-            print(td)
-
-np.savetxt("./data.csv", data, delimiter=",")
+model = Model()
+model.load()
---
+++ model.py
@@ -0,0 +1,117 @@
+
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+from sklearn.preprocessing import PolynomialFeatures
+from tensorflow import keras
+import matplotlib.pyplot as plt
+import numpy as np
+import pickle
+
+class Model :
+    def __init__(self, data_file_path="./data.csv", data_length=5, scaler_file_path="scaler.pickle", model_file_path="./best_lstm.h5") :
+        self.data_file_path = data_file_path
+        self.data_length = data_length
+
+        self.scaler_file_path = scaler_file_path
+        self.scaler_list = []
+
+        self.model_file_path = model_file_path
+        self.history = None
+
+    def __replace(self) :
+        # load data
+        load_data = np.loadtxt(self.data_file_path, delimiter=",")
+
+        # group data by each same players
+        label = load_data[:, 0]
+        data = load_data[:, 1:]
+        data = np.split(data, np.sort(np.unique(label, return_index=True)[1]))
+
+        data.pop(0)
+
+        # make time-series data
+        new_data = []
+
+        for i, batch in enumerate(data) :
+            if batch.shape[0] > self.data_length :
+                splited_batch = np.split(np.flip(batch, axis=0), np.array([(i, i+self.data_length + 1) for i in range(0, batch.shape[0] - self.data_length)]).reshape(-1))
+                splited_batch = list(map(lambda x : x.tolist(), splited_batch))
+
+                for j, _ in enumerate(splited_batch) :
+                    splited_batch.pop(j)
+
+                new_data += splited_batch
+
+        new_data = np.array(new_data)
+
+        # scale data
+        for i in range(new_data.shape[-1]) :
+            scaler = StandardScaler()
+
+            new_data_shape = new_data[:, :, i].shape
+            new_data[:, :, i] = scaler.fit_transform(new_data[:, :, i].flatten().reshape(-1, 1)).reshape(new_data_shape)
+
+            self.scaler_list.append(scaler)
+
+        # save scaler list
+        with open(self.scaler_file_path, "wb") as fp :
+            pickle.dump(self.scaler_list, fp)
+
+        return new_data
+
+    def load(self) :
+        self.model = keras.models.load_model(self.model_file_path)
+        
+        with open(self.scaler_file_path, "rb") as fp :
+            self.scaler_list = pickle.load(fp)
+
+    def train(self) :
+        data = self.__replace(file_path="./data.csv", data_length=5)
+
+        train_x = data[:, :-1]
+        train_t = data[:, -1, 0]
+
+        # split train data
+        train_input, test_input, train_target, test_target = train_test_split(train_x, train_t, test_size=0.2, random_state=2022)
+
+        # make model (default)
+        self.model = keras.Sequential([
+            keras.layers.LSTM(50, input_shape=(train_input.shape[1], train_input.shape[2]), return_sequences=True),
+            keras.layers.LSTM(64),
+            keras.layers.Dense(100, activation="relu"),
+            keras.layers.Dense(1, activation="linear")
+        ])
+
+        self.model.compile(
+            optimizer="adam",
+            loss="mse",
+        )
+
+        check_point = keras.callbacks.ModelCheckpoint(self.model_file_path)
+        # early_stopping = keras.callbacks.EarlyStopping(patience=3, restore_best_weights=True)
+
+        self.model.summary()
+
+        # fit model
+        self.history = self.model.fit(
+            train_input,
+            train_target,
+            epochs=100,
+            batch_size=10,
+            validation_data=(test_input, test_target),
+            callbacks=[check_point]
+        )
+
+    def show(self) :
+        plt.plot(self.history.history["loss"])
+        plt.plot(self.history.history["val_loss"])
+        plt.xlabel("epoch")
+        plt.xlabel("loss")
+        plt.legend(["train", "val"])
+        plt.show()
+
+    def predict(self, predict_input) :
+        predict = self.model.predict(predict_input)
+        scaler = self.scaler_list[0]
+
+        return scaler.inverse_transform(predict)
